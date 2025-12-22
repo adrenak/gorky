2bee8f368bb13fd4b202a4a6f2f0dad7b7f1fcd7
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2025-12-22 11:14:08 +0530

    Update template config

--- template/site-config.js
+++ template/site-config.js
@@ -1,8 +1,8 @@
 // Site Configuration for Gorky Documentation Site
 module.exports = {
-    baseUrl: 'https://adrenak.github.io/gorky',
+    baseUrl: 'https://www.github.com', // Enter your website URL here
     siteName: 'Gorky',
-    authorName: 'Vatsal Ambastha',
+    authorName: 'Your name',
     defaultDescription: 'Gorky - A lightweight, markdown-powered static site generator for creating beautiful blogs and personal websites. Perfect for GitHub Pages deployment.',
     defaultKeywords: 'gorky, static site generator, markdown, blog, GitHub Pages, JAMstack, static website, markdown blog',
     favicon: 'favicon.ico',
